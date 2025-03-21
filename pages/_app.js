b5f71899efeb593e012ac138a6971ed846f18839
israel mvono <israelmvono@gmail.com>
2025-03-21 22:16:21 +0300

    Update _app.js to include print stylesheet and meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
 import MainLayout from '../components/layout/MainLayout';
 import NextNProgress from 'nextjs-progressbar';
+import Head from 'next/head';
 import '../styles/globals.css';
+import '../styles/print.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   // Check if the page is an auth page (login, register, etc.)
@@ -11,6 +13,10 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Blood Bank Management System</title>
+        </Head>
         <CSSReset />
         <NextNProgress color="#3182CE" options={{ showSpinner: false }} />
         
