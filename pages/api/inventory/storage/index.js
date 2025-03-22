557103e9040e69b32d069577020252a269898b2a
israel mvono <israelmvono@gmail.com>
2025-03-22 03:11:04 +0300

    Fix storage API to use dbConnect instead of mongodb connection

--- pages/api/inventory/storage/index.js
+++ pages/api/inventory/storage/index.js
@@ -1,4 +1,4 @@
-import dbConnect from '../../../../lib/mongodb';
+import dbConnect from '../../../../lib/dbConnect';
 import Storage from '../../../../models/Storage';
 import StorageLog from '../../../../models/StorageLog';
 import withAuth from '../../../../lib/middlewares/withAuth';
